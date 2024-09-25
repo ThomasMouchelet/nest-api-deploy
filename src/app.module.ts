283776b19d21709e88fa678a5966ff96ef914572
Thomas Mouchelet <mouchelet.thomas@gmail.com>
2024-09-25 08:46:25 +0200

    feat: add r post

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostModule } from './post/post.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forFeature(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRESQL_ADDON_HOST,
+      port: parseInt(process.env.POSTGRESQL_ADDON_PORT),
+      username: process.env.POSTGRESQL_ADDON_USER,
+      password: process.env.POSTGRESQL_ADDON_PASSWORD,
+      database: process.env.POSTGRESQL_ADDON_DB,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      extra: {
+        min: 2,
+        max: 5,
+      },
+    }),
+    PostModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
